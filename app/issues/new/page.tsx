8566c85f8bc05d54cb592573eac16114ed5e4b3e
BaluNeu <mundlamuri.b@northeastern.edu>
2023-12-28 21:36:21 -0500

    build the new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,20 @@
-import { TextField } from '@radix-ui/themes'
+'use client';
+// making this as a client component
+import { TextArea, TextField } from '@radix-ui/themes'
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const NewIssuePage = () => {
   return (
-    <div>
+    <div className='max-w-xl space-y-3'>
 
-    <TextField.Root>
+    <TextField.Root >
         <TextField.Input placeholder='Title'/>
+        
     </TextField.Root>
+
+    <TextArea placeholder='Description'/>
+    <Button> Submit New Issue</Button>
     </div>
   )
 }
